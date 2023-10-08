4b5d15a4b0cbc5cef6db3fda57de8e45a87f3e46
Juan Carlos Silva <caco_deleon@hotmail.com>
2023-10-08 18:52:36 -0300

    Actualización: update

--- app/src/main/java/com/example/nasaapp/ActivityHome.java
+++ app/src/main/java/com/example/nasaapp/ActivityHome.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityHome extends AppCompatActivity {
@@ -14,6 +16,7 @@ public class ActivityHome extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         Button btnCerrarSesion = findViewById(R.id.button);
+        ImageView imageViewLogo9 = findViewById(R.id.imageViewLogo9);
 
         btnCerrarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +24,14 @@ public class ActivityHome extends AppCompatActivity {
                 Intent intent = new Intent(ActivityHome.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
+            }
+        });
+
+        imageViewLogo9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ActivityHome.this, Guides.class);
+                startActivity(intent);
             }
         });
     }
