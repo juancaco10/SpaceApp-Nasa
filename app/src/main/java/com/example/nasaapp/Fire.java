826a5402b95ad700902df3f433877c136d64e7fb
Juan Carlos Silva <caco_deleon@hotmail.com>
2023-10-10 08:23:38 -0300

    Cambios Diseño

--- app/src/main/java/com/example/nasaapp/Fire.java
+++ app/src/main/java/com/example/nasaapp/Fire.java
@@ -22,6 +22,6 @@ public class Fire extends AppCompatActivity {
         webSettings.setJavaScriptEnabled(true);
 
         // Carga la URL del juego
-        webView.loadUrl("https://firms.modaps.eosdis.nasa.gov/map/#d:today;@-55.6,-32.1,6.2z");
+        webView.loadUrl("https://worldview.earthdata.nasa.gov/?v=-82.7544375,-39.852265625,-34.730999999999995,-18.090546875&e=true&efs=false&efa=true&efd=2023-06-12,2023-10-10&efc=wildfires&l=Reference_Labels_15m(hidden),Reference_Features_15m(hidden),Coastlines_15m,BlueMarble_NextGeneration,VIIRS_NOAA20_CorrectedReflectance_TrueColor(hidden),VIIRS_SNPP_CorrectedReflectance_TrueColor(hidden),MODIS_Aqua_CorrectedReflectance_TrueColor(hidden),MODIS_Terra_CorrectedReflectance_TrueColor&lg=true&s=-55.5435,-30.905&t=2023-10-10-T10%3A32%3A51Z");
     }
 }
