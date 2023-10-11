76cdb5bb7f9da80116a810b0ddf817807d769899
Juan Carlos Silva <caco_deleon@hotmail.com>
2023-10-11 19:25:06 -0300

    Comit-update

--- app/src/main/java/com/example/nasaapp/LoginActivity.java
+++ app/src/main/java/com/example/nasaapp/LoginActivity.java
@@ -5,7 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,11 +29,39 @@ public class LoginActivity extends AppCompatActivity {
     private EditText editTextText3;
     private EditText editTextText4;
     private Button loginButton;
+    private static final int RC_SIGN_IN = 9001; // Puedes elegir cualquier número
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Tu código existente para los elementos de la interfaz de usuario
+
+        ImageButton googleSignInButton = findViewById(R.id.googlebtn);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id)) // Reemplaza con tu ID de cliente de Firebase
+                .requestEmail()
+                .build();
+
+        GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(ConnectionResult connectionResult) {
+                        // Manejar errores de conexión
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        googleSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+        });
 
         editTextText3 = findViewById(R.id.editTextText3);
         editTextText4 = findViewById(R.id.editTextText4);
